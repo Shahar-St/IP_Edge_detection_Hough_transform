32a427b81b6680eaeae16906a8833fe6089e0a75
Shahar Stahi <shaharstahi@gmail.com>
2021-12-25 17:08:14 +0200

    Added task 1

--- hw5_functions.py
+++ hw5_functions.py
@@ -1,2 +1,26 @@
+import numpy as np
+from scipy.signal import convolve2d
+
+
 def print_IDs():
     print("305237257+312162027\n")
+
+
+def calculate_sobel_edge_detection(img):
+    img = np.array(img, dtype=float)
+
+    sobel_matrix_x_direction = np.array([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ])
+    x_edges = convolve2d(img, sobel_matrix_x_direction, mode='same')
+
+    sobel_matrix_y_direction = np.array([
+        [-1, -2, -1],
+        [0, 0, 0],
+        [1, 2, 1]
+    ])
+    y_edges = convolve2d(img, sobel_matrix_y_direction, mode='same')
+
+    return np.sqrt(np.square(x_edges) + np.square(y_edges))
--- hw5_script.py
+++ hw5_script.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import cv2.cv2 as cv2
+
 from hw5_functions import *
 
 
@@ -6,6 +9,19 @@ def main():
     print_IDs()
 
     print("-----------------------Task 1----------------------\n")
+    balls1_img = cv2.cvtColor(cv2.imread(r'images/balls1.tif'), cv2.COLOR_BGR2GRAY)
+
+    balls1_sobel_edges = calculate_sobel_edge_detection(balls1_img)
+    threshold = 90
+    _, bw_img = cv2.threshold(balls1_sobel_edges, threshold, 255, cv2.THRESH_BINARY)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.title('Original')
+    plt.imshow(balls1_img, cmap='gray', vmin=0, vmax=255)
+    plt.subplot(1, 2, 2)
+    plt.title('Sobel Image')
+    plt.imshow(bw_img, cmap='gray', vmin=0, vmax=255)
 
     print("-----------------------Task 2----------------------\n")
 
@@ -14,6 +30,8 @@ def main():
     print("-----------------------Task 4----------------------\n")
 
     print("-----------------------Task 5----------------------\n")
+
+    plt.show()
 
 
 if __name__ == '__main__':
